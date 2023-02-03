df3c854897ace198d826da2ffc30a4f6c3626e4f
monamankar <monisk2228@gmail.com>
2023-02-04 00:42:08 +0530

    added

--- src/userregister.js
+++ src/userregister.js
@@ -1,63 +1,36 @@
-import { useState } from 'react';
+
 import './App.css';
 
 
 function UserRegister(){
 
-  const [name,setName]=useState();
-  const [mobileno,setMobileno]=useState();
-  const [email,setEmail]=useState();
-  const [password,setPassword]=useState();
-  const [gender,setGender]=useState();
-  const [dateob,setDateob]=useState();
-  const [hobbies,setHobbies]=useState([]);
-  const [address,setAddress]=useState();
-  const [pincode,setPincode]=useState();
-  const [city,setCity]=useState();
-  const [states,setStates]=useState();
-  const [image,setImage]=useState();
-  const [userdata,setUserData]=useState([]);
-
-  function submitData(){
-    console.log("User Sign-up Details");
-    let obj = { name,mobileno,email,password,gender,dateob,hobbies,address,
-              pincode,city,states,image};
-    userdata.push(obj);
-    setUserData([...userdata]);
-    console.log(userdata);
-    }
-  
-
-  
-
-
   return(
     <>
-      <h1>Registration with Validation</h1>
+      <h1>Registration</h1>
 
-      Name: <input id='1' type = "text" onChange={(e)=>{setName(e.target.value)}}/><br></br>
+      Name: <input id='1' type = "text"/><br></br>
 
-      Mobile No: <input id='2' type = "text" onChange={(e)=>{setMobileno(e.target.value)}}/><br></br>
+      Mobile No: <input id='2' type = "text"/><br></br>
 
-      Email:<input id='3' type = "email" onChange={(e)=>{setEmail(e.target.value)}}/><br></br>
+      Email:<input id='3' type = "email"/><br></br>
 
-      Password:<input id='4' type = "password" onChange={(e)=>{setPassword(e.target.value)}}/><br></br>
+      Password:<input id='4' type = "password"/><br></br>
 
-      Gender:Female<input id='5' type = "radio" name="gender" onChange={(e)=>{setGender("Female")}}/> 
-             Male<input id='6' type="radio" name="gender" onChange={(e)=>{setGender("Male")}}/><br></br>
+      Gender:Female<input id='5' type = "radio" name="gender"/> 
+             Male<input id='6' type="radio" name="gender"/><br></br>
 
-      DoB:<input id='7' type = "date" onChange={(e)=>{setDateob(e.target.value)}}/><br></br>
+      DoB:<input id='7' type = "date"/><br></br>
 
-      Hobbies:<input id='8' type = "checkbox" name="hobbies" onChange={(e)=>{setHobbies(e,"Singing")}}/>Singing
-              <input id='9' type = "checkbox" name="hobbies" onChange={(e)=>{setHobbies(e,"Dancing")}}/>Dancing
-              <input id='10' type = "checkbox" name="hobbies" onChange={(e)=>{setHobbies(e,"Pottery")}}/>Pottery<br></br>
+      Hobbies:<input id='8' type = "checkbox" name="hobbies"/>Singing
+              <input id='9' type = "checkbox" name="hobbies"/>Dancing
+              <input id='10' type = "checkbox" name="hobbies"/>Pottery<br></br>
 
-      Address:<input id='11' type = "text" onChange={(e)=>{setAddress(e.target.value)}}/><br></br>
+      Address:<input id='11' type = "text"/><br></br>
 
-      Pincode:<input id='12' type="text" onChange={(e)=>{setPincode(e.target.value)}}/><br></br>
+      Pincode:<input id='12' type="text"/><br></br>
 
       <label htmlFor="City">Choose City:</label>
-      <select name="city" id="13" onChange={(e)=>{setCity(e.target.value)}}>
+      <select name="city" id="13">
         <option value="nagpur">Nagpur</option>
         <option value="wardha">Wardha</option>
         <option value="bhandara">Bhandara</option>
@@ -66,11 +39,11 @@ function UserRegister(){
       
       <br></br>
 
-      State:<input id='14' type = "text"  onChange={(e)=>{setStates(e.target.value)}}/><br></br>
+      State:<input id='14' type = "text"/><br></br>
 
-      Image:<input id='15' type = "file" onChange={(e)=>{setImage(e)}}/><br></br>
+      Image:<input id='15' type = "file"/><br></br>
 
-      <input id='16' type = "submit" onClick={()=>{submitData()}}/><br></br>
+      <input id='16' type = "submit"/><br></br>
     </>
     );
   }
--- src/usersignin.js
+++ src/usersignin.js
@@ -26,7 +26,7 @@ import { signInSchema } from "./schemas/signinschema";
 
 
     return(
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="container">
         <div className="mb-3">
           <label htmlFor="name" className="form-label">Name</label>
@@ -36,23 +36,23 @@ import { signInSchema } from "./schemas/signinschema";
         </div>
 
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="email" name="email"
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="email" name="email"
            value={values.email} onChange={handleChange} onBlur={handleBlur}/>
            {errors.email && touched.email ? (<p className="form-error">{errors.email}</p>) : null}
           <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
         </div>
 
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" placeholder="password" name="password"
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" id="password" placeholder="password" name="password"
            value={values.password} onChange={handleChange} onBlur={handleBlur}/>
            {errors.password && touched.password ? (<p className="form-error">{errors.password}</p>) : null}
         </div>
 
         <div className="mb-3">
-          <label htmlFor="exampleConfirmPassword1" className="form-label">Confirm Password</label>
-          <input type="confirmpassword" className="form-control" id="exampleConfirmPassword1" placeholder="confirm_password" name="confirm_password"  
+          <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
+          <input type="confirmpassword" className="form-control" id="confirmPassword" placeholder="confirm_password" name="confirm_password"  
           value={values.confirm_password} onChange={handleChange} onBlur={handleBlur}/>
           {errors.confirm_password && touched.confirm_password ? (<p className="form-error">{errors.confirm_password}</p>) : null}
         </div>
@@ -60,6 +60,7 @@ import { signInSchema } from "./schemas/signinschema";
         <button type="submit" className="btn btn-primary">Submit</button>
         </div>
       </form>
-    )
+   
+   )
 }
 export default UserSignIn;
